a0980ac282c6ea79fdb7f2448e8c2adb177fcb83
mitch <mitchell.heaton@gmail.com>
2017-11-07 17:51:31 +1100

    initial add

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class HealingPotion
+    {
+        public int ID { get; set; }
+        public int AmountHeal { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+    }
+}
---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player _player;
+        public SuperAdventure()
+        {
+            InitializeComponent();
+
+            // init player
+            _player = new Player
+            {
+                MaxHitPoints = 10,
+                CurrentHitPoints = 10,
+                Gold = 20,
+                ExperiencePoints = 0,
+                Level = 1
+            };
+
+            // set labels to player stats
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+    }
+}
